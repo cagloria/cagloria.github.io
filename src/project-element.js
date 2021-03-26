b3db14a6912cb11d8ea8dc5d9c0ed562fb9b930a
cagloria <33134245+cagloria@users.noreply.github.com>
2021-03-26 14:10:27 -0700

    Added button link class

--- src/project-element.js
+++ src/project-element.js
@@ -17,27 +17,13 @@ class ProjectElement extends HTMLDivElement {
             code === undefined
                 ? ""
                 : `
-            <a href="${code}" class="icon-link">
-                <ion-icon
-                    name="logo-github"
-                    class="icon-link__icon"
-                    aria-hidden="true"
-                ></ion-icon>
-                <div class="icon-link__cover"></div>
-                <span class="icon-link__text">Code</span>
-            </a>`;
+            <a href="${code}" class="button-link">GitHub</a>`;
 
         let previewElement =
             preview === undefined
                 ? ""
                 : `
-            <a href="${preview}" class="icon-link icon-link--shorter-translate">
-                <span class="material-icons icon-link__icon" aria-hidden="true">
-                    play_circle
-                </span>
-                <div class="icon-link__cover"></div>
-                <span class="icon-link__text">Preview</span>
-            </a>`;
+            <a href="${preview}" class="button-link">Live Demo</a>`;
 
         this.innerHTML = `
             <img src="${image}" alt="${alt}" class="project__image" />
