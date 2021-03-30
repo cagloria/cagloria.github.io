592f1f15ba0c5d9069182bd859433d7f1f409f16
cagloria <33134245+cagloria@users.noreply.github.com>
2021-03-29 20:28:05 -0700

    Added aria attributes and scripts for nav

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 window.onload = function () {
     const navButton = document.getElementById("nav-button");
+    const navLinks = document.querySelectorAll("nav a");
+
     navButton.addEventListener("click", toggleNav);
+    navLinks.forEach((link) => {
+        link.setAttribute("tabindex", "-1");
+    });
 };
 
 function toggleNav() {
-    const openNavClass = "open-nav";
     const header = document.getElementsByTagName("header")[0];
+    const navButton = document.getElementById("nav-button");
+    const navLinks = document.querySelectorAll("nav a");
+    const openNavClass = "open-nav";
 
     header.classList.toggle(openNavClass);
 
-    // if (header.classList.contains(openNavClass)) {
-    //     header.classList.remove(openNavClass);
-    // } else {
-    //     header.classList.add(openNavClass);
-    // }
+    if (header.classList.contains(openNavClass)) {
+        navButton.setAttribute("aria-expanded", "true");
+        navButton.setAttribute("aria-label", "Close navigation");
+        navLinks.forEach((link) => {
+            link.setAttribute("tabindex", "0");
+        });
+    } else {
+        navButton.setAttribute("aria-expanded", "false");
+        navButton.setAttribute("aria-label", "Open navigation");
+        navLinks.forEach((link) => {
+            link.setAttribute("tabindex", "-1");
+        });
+    }
 }
