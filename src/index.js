d314de62435834b5b45f65ee07f6a4096fc5389d
cagloria <33134245+cagloria@users.noreply.github.com>
2021-11-16 21:16:13 -0700

    Implementing new design, updated intro and header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import { setupHeader, closeNavOnScroll } from "./scripts/header.js";
+import { closeNavOnScroll } from "./scripts/header.js";
 import { loadProjects } from "./scripts/projects.js";
 
 window.onload = function () {
-    setupHeader();
     loadProjects();
 };
 
