ec751cac17f4db4f3ed7079698c76d2f28ae0efc
cagloria <33134245+cagloria@users.noreply.github.com>
2021-11-19 13:08:33 -0700

    Upgraded hiding nav when scrolling down on mobile

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import { closeNavOnScroll } from "./scripts/header.js";
 import { loadProjects } from "./scripts/projects.js";
+import { addMediaQuery, changeHeader } from "./scripts/header.js";
 
 window.onload = function () {
     loadProjects();
+    addMediaQuery();
 };
 
 window.onscroll = function () {
-    closeNavOnScroll();
+    changeHeader();
 };
